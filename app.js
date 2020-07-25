86bd28d037c2b885535e79768cf2f1d07b1ccfe7
MuzhdahWaseem <64963555+MuzhdahWaseem@users.noreply.github.com>
2020-07-25 18:54:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var input = document.querySelector('input');
+var btn = document.querySelector('.addTask > button');
+
+btn.addEventListener('click' , addList);
+input.addEventListener('keyup' , (e) =>{
+    (e.keyCode === 13 ? addList(e):null)
+})
+
+
+function addList(e){
+    var notCompleted = document.querySelector('.notCompleted');
+    var completed = document.querySelector('.completed');
+    var newLi = document.createElement('li');
+    var checkBtn = document.createElement('button');
+    var delBtn = document.createElement('button');
+
+    checkBtn.innerHTML ='<i class="fa fa-check"></i>';
+    delBtn.innerHTML ='<i class="fa fa-trash"></i>';
+
+
+
+    if (input.value !==''){
+        newLi.textContent = input.value;
+        input.value = '';
+        notCompleted.appendChild(newLi);
+        newLi.appendChild(checkBtn);
+        newLi.appendChild(delBtn);
+    }
+    checkBtn.addEventListener('click' , function (){
+        var parent = this.parentNode;
+        parent.remove()
+
+        completed.appendChild(parent);
+        checkBtn.style.display = 'none'
+    });
+
+    delBtn.addEventListener('click' , function (){
+    var parent = this.parentNode;
+    parent.remove();
+
+});
+}
